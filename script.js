98fd3b5991587ebcee026aa9107803cc10a50e68
Brendan <brendancarter43@gmail.com>
2018-06-25 19:55:10 -0500

    landing page w/ transitions

---
+++ script.js
@@ -0,0 +1,23 @@
+var backgroundEnter = document.getElementsByClassName("backdrop-img");
+
+
+for (let index = 0; index < backgroundEnter.length; index++) {
+    const element = backgroundEnter[index];
+
+    element.addEventListener("mouseenter", mouseEnter);
+    element.addEventListener("mouseleave", mouseLeave);
+
+    var c = document.getElementsByClassName("article-info");
+    
+    function mouseEnter() {
+        backgroundEnter[index].style.filter ="brightness(100%)";
+        c[index].style.visibility = "visible";
+        c[index].style.opacity = "1";
+    }
+    
+    function mouseLeave() {
+        backgroundEnter[index].style.filter ="brightness(55%)";
+        c[index].style.visibility = "hidden";
+        c[index].style.opacity = "0";
+    }
+}
